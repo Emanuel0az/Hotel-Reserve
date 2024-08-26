984674ab663d1c2834a06ec96d9dc616dd792e29
Emanuel0az <168204486+Emanuel0az@users.noreply.github.com>
2024-08-26 15:51:12 -0600

    views

--- Hotel.sql
+++ Hotel.sql
@@ -2,8 +2,44 @@ CREATE DATABASE Hotel
 
 use hotel
 
-CREATE Table usuarios(
-    id int primary key auto_increment,
-    names varchar(50) not null,
-    edad INT not null
+CREATE TABLE Clientes (
+    Num_Cedula VARCHAR(100) NOT NULL PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
 );
+CREATE TABLE Trabajador (
+    Num_Cedula VARCHAR(100) NOT NULL PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Tipo_Trabajador ENUM('Recepcionista', 'Miselaneo', 'Botones')
+);
+CREATE TABLE Administradores (
+    Num_Cedula VARCHAR(100) NOT NULL PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Tipo_Administrador ENUM('G_General', 'G_Recepcion', 'G_Ventas_y_Marketing',
+                            'G_Recursos_Humanos', 'G_Finanzas', 'G_Eventos',
+                            'G_Mantenimiento', 'G_Seguridad')
+);
+CREATE TABLE Habitaciones (
+    id_habitacion INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    Disponibilidad ENUM('Disponible', 'Ocupado', 'En limpieza', 'En mantenimiento', 'Otros'),
+    Tipo_Habitacion VARCHAR(100) NOT NULL,
+    Capacidad INT NOT NULL,
+    Precio DECIMAL(10,2) NOT NULL
+);
+
+ALTER TABLE Habitaciones
+MODIFY COLUMN Tipo_Habitacion ENUM('Individual', 'Doble', 'Triple', 'Cuádruple', 'Suite') NOT NULL;
+
+CREATE TABLE Reservas (
+    id_reserva INT PRIMARY KEY AUTO_INCREMENT,
+    fecha_reserva DATE NOT NULL,
+    fecha_entrada DATE NOT NULL,
+    fecha_salida DATE NOT NULL,
+    id_habitacion INT NOT NULL,
+    Num_Cedula_Cliente VARCHAR(100) NOT NULL,
+    Foreign Key (id_habitacion) REFERENCES Habitaciones(id_habitacion),
+    Foreign Key (Num_Cedula_Cliente) REFERENCES Clientes(Num_Cedula)
+);
+
+drop DATABASE hotel
